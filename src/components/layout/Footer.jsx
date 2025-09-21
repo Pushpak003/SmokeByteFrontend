1b6db78e16061a5cfea754b948bc95ceccfa11cb
Pushpak003 <pushpakpathe63@gmail.com>
2025-09-21 22:52:11 +0530

    changed a file

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -6,6 +6,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <p>&copy; {new Date().getFullYear()} SmokeByte. All Rights Reserved.</p>
+        <p> By Pushpakk😎 </p>
       <nav>
         <Link to="/privacy">Privacy Policy</Link>
         <Link to="/terms">Terms of Service</Link>
